70304560d89a071682192326d95c07cc16805269
kdv1995 <tribeofdanel@gmail.com>
2022-03-07 23:34:54 +0200

    section-us

---
+++ src/components/Main/SectionUs/SectionUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SectionUsTitle from "./SectionUsTitle/SectionUsTitle";
+
+const SectionUs = () => {
+  return (
+    <section className="SectionUs">
+      <div className="SectionContainerUs">
+        <SectionUsTitle />
+      </div>
+    </section>
+  );
+};
+
+export default SectionUs;
---
+++ src/components/Main/SectionUs/SectionUsTitle/SectionUsTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SectionUsTitle = () => {
+  return (
+    <div>
+      <h2 className="UsTitle">Про нас</h2>
+    </div>
+  );
+};
+
+export default SectionUsTitle;

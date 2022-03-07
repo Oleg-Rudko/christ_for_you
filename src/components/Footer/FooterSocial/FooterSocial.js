b4fbf87b3379f7ac394f28dd08cfc64b517bec5d
kdv1995 <tribeofdanel@gmail.com>
2022-03-07 23:51:27 +0200

    footer

--- src/components/Footer/FooterSocial/FooterSocial.js
+++ src/components/Footer/FooterSocial/FooterSocial.js
@@ -1,5 +1,14 @@
+import React from "react";
 const FooterSocial = () => {
-  return <div></div>;
+  return (
+    <div className="FooterSocialFlex">
+      <ul className="FooterSocialMenu">
+        <li className="FooterSocialItems">
+          <a className="FooterSocialLinks"></a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default FooterSocial;

4b60e35b59274350b33a7f3d2dd8fb8a92b39475
kdv1995 <tribeofdanel@gmail.com>
2022-03-14 18:02:51 +0200

    finished

--- src/components/Main/SectionMain/CallBack/CallBack.js
+++ src/components/Main/SectionMain/CallBack/CallBack.js
@@ -1,18 +1,20 @@
+import React, { useState } from "react";
 import CallBackIcon from "../../../Footer/FooterCall/FooterIcons/FooterIconCall.svg";
+import CallbackForm from "../../../CallbackForm/CallbackForm";
 const CallBack = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="CallBackBlockFlex">
-      <div className="CallBackBlock">
-        <a href="/" className="CallBackNumber">
-          <img
-            width="22px"
-            height="22px"
-            className="CallBackSvg"
-            src={CallBackIcon}
-            alt=""
-          ></img>
-          Дізнатись телефон
-        </a>
+      <div className="CallBackBlock" onClick={() => setIsOpen(!isOpen)}>
+        <img
+          width="22px"
+          height="22px"
+          className="CallBackSvg"
+          src={CallBackIcon}
+          alt=""
+        ></img>
+        Дізнатись телефон
+        <CallbackForm isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
     </div>
   );
--- src/components/Tracts/SectionTractsItems/SectionTractsItems.js
+++ src/components/Tracts/SectionTractsItems/SectionTractsItems.js
@@ -4,32 +4,46 @@ const SectionTractsItems = () => {
       <div className="SectionTractsContainer">
         <ul className="TractsMenuOne">
           <li className="TractsMenuOneItems">
-            <a href="#" className="TractsMenuLinks"></a>
+            <a href="/" className="TractsMenuLinks">
+              Tracts
+            </a>
           </li>
           <li className="TractsMenuOneItems">
-            <a href="#" className="TractsMenuLinks"></a>
+            <a href="/" className="TractsMenuLinks">
+              Tracts
+            </a>
           </li>
           <li className="TractsMenuOneItems">
-            <a href="#" className="TractsMenuLinks">
+            <a href="/" className="TractsMenuLinks">
               Дізнайся більше про себе та своє спасіння
             </a>
           </li>
           <li className="TractsMenuOneItems">
-            <a href="#" className="TractsMenuLinks"></a>
+            <a href="/" className="TractsMenuLinks">
+              Tracts
+            </a>
           </li>
         </ul>
         <ul className="TractsMenuTwo">
           <li className="TractsMenuTwoItems">
-            <a href="#" className="TractsMenuLinks"></a>
+            <a href="/" className="TractsMenuLinks">
+              Tracts
+            </a>
           </li>
           <li className="TractsMenuTwoItems">
-            <a href="#" className="TractsMenuLinks"></a>
+            <a href="/" className="TractsMenuLinks">
+              Tracts
+            </a>
           </li>
           <li className="TractsMenuTwoItems">
-            <a href="#" className="TractsMenuLinks"></a>
+            <a href="/" className="TractsMenuLinks">
+              Tracts
+            </a>
           </li>
           <li className="TractsMenuTwoItems">
-            <a href="#" className="TractsMenuLinks"></a>
+            <a href="/" className="TractsMenuLinks">
+              Tracts
+            </a>
           </li>
         </ul>
       </div>
